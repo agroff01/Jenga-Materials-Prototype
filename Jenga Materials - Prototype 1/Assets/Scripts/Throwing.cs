76a5373f004d663159906eb23b53ca5dda34c8b0
tristynl <tristyn-lai@outlook.com>
2023-10-14 16:27:42 -0700

    Made Throwing Objects (in Tristyn's Scene)

--- Jenga Materials - Prototype 1/Assets/Scripts/Throwing.cs
+++ Jenga Materials - Prototype 1/Assets/Scripts/Throwing.cs
@@ -5,15 +5,68 @@ using TMPro;
 
 public class Throwing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  
+    [Header("References")]
+    public Transform cam;
+    public Transform attackPoint;
+    public GameObject objectToThrow;
+
+    [Header("Settings")]
+    public int totalThrows;
+    public float throwCooldown;
+
+    [Header("Throwing")]
+    public KeyCode throwKey = KeyCode.Mouse0;
+    public float throwForce;
+    public float throwUpwardForce;
+
+    bool readyToThrow;
+
+    private void Start()
     {
-        
+        readyToThrow = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0){
+            Throw();
+        }
+    }
+
+    private void Throw()
+    {
+        readyToThrow = false;
+
+        //Create object that will be thrown
+        GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
+
+        //Get rigidbody component
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        //Calculate direction
+        Vector3 forceDirection = cam.transform.forward;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(cam.position, cam.forward, out hit, 500f))
+        {
+            forceDirection = (hit.point - attackPoint.position).normalized;
+        }
+
+        //Add force
+        Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
+
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        totalThrows--;
+
+        //Implement throwCooldown
+        Invoke(nameof(ResetThrow), throwCooldown);
+    }
+
+    private void ResetThrow()
+    {
+        readyToThrow = true;
     }
 }
